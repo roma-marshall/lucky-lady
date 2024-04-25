72c218048997f0c2c97130f0bd2fd2243be27b15
roma_marshall <roma_marshall@outlook.com>
2024-04-25 19:40:00 +0200

    code clean up

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -32,10 +32,4 @@
 
 <script setup>
 import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from '@heroicons/vue/20/solid'
-
-const items = [
-  { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-  { id: 2, title: 'Front End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
-  { id: 3, title: 'User Interface Designer', department: 'Design', type: 'Full-time', location: 'Remote' },
-]
 </script>
